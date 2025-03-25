4cb0ddf05c590d9b34efb87173be19ef05031a7c
ngocphuc.1910 <ngocphuc159tl@gmail.com>
2025-03-25 22:56:16 +0700

    show up commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const h1 = <h1>Trying to learn React</h1>
+const h1 = <h1>Trying to learn React</h1>;
+const myArticle = <article>This is my article content.</article>;
+
+
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// Write code here:
+const myDiv = (
+  <div className ="big">I AM A BIG DIV</div>
+)
